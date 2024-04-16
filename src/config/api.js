79311e285c98cde7c03a169c8b4df81727397fff
José Rafael <rafaelmatiashue@gmail.com>
2024-04-16 13:45:43 -0300

    teste

--- src/config/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-   //baseURL: 'http://localhost:3333'
-  //BASE DA API ON-LINE baseURL: 'https://api-neobet.onrender.com'
-  baseURL: 'https://api-neobet.onrender.com'
-}); 
